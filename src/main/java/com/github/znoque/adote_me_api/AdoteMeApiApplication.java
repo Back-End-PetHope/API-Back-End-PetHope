88aac75afbb6d7a2387688104e2469b6df3afc3f
Enoque <Enoque@DESKTOP-CJMG6TE.mshome.net>
2025-02-13 22:27:23 -0300

    1. Criação do projeto base via "https://start.spring.io".

---
+++ src/main/java/com/github/znoque/adote_me_api/AdoteMeApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.znoque.adote_me_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoteMeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoteMeApiApplication.class, args);
+	}
+
+}
