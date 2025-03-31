4157e6fd7107b6fd4cc7702fdb92ef62c24a1faa
Thales Oliveira S. Costa <thalesslva67@gmail.com>
2025-03-30 21:35:15 -0300

    refactor: alteração na classe service, excluindo os atributos: doença, necessidade especial, temperamento e porte. E adicionando coluna de descrição.

--- src/main/java/com/github/znoque/pethope/dto/PetDto.java
+++ src/main/java/com/github/znoque/pethope/dto/PetDto.java
@@ -1,16 +1,12 @@
 package com.github.znoque.pethope.dto;
 
 import com.github.znoque.pethope.model.pet.Especie;
-import com.github.znoque.pethope.model.pet.Porte;
 
 public record PetDto(
         String nome,
         Especie especie,
         int idade,
-        Porte porte,
-        String temperamento,
-        String doenca,
-        String necessidadeEspecifica,
+        String descricao,
         boolean ativo,
         boolean disponibilidade) {
 
